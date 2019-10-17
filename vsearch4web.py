3b497b340db7d2ee109776866dacecdde06a765f
Shorya <sshaurya43@gmail.com>
2019-10-17 15:45:09 +0530

    boom

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,8 +1,14 @@
 
 
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,escape
 from myfile import search4letters
 app = Flask(__name__)
+
+def log_request(req : 'Flask_request' , res  : str) -> None:
+    with open('myfile.log', 'a') as log:
+
+
+        print(req.form,req.remote_addr, req.user_agent,res, file=log, sep='|')
 
 
 @app.route('/search4', methods=['POST'])
@@ -12,6 +18,7 @@ def do_search()->'html':
 
     title = 'Here are your results : '
     results = str(search4letters(phrase , letters))
+    log_request(request, results)
 
     return render_template('results.html', the_phrase = phrase , the_letters = letters , the_title = title , the_results = results,)
 @app.route('/')
@@ -19,5 +26,19 @@ def do_search()->'html':
 @app.route('/entry')
 def entry_page()-> 'html':
     return render_template('entry.html',the_title = 'Welcome to search4letters on the web!')
+@app.route('/viewlog')
+def view_log() -> 'html':
+    contents = []
+    with open('myfile.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data' , 'Remote_addr', 'Use_agent' , 'Results')
+    return render_template('viewlog.html' ,
+                           the_title = 'View Log' ,
+                           the_row_titles = titles,
+                           the_data = contents, )
+
 
 app.run(debug=True)
